62821ee12acde626ba601dca221a605694991662
Latifa <latifaloufi7@gmail.com>
2024-02-28 15:08:50 +0300

    Update registration page layout and styling

--- src/Register.js
+++ src/Register.js
@@ -1,36 +1,60 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import { useNavigate } from 'react-router-dom'; 
 import styles from './CommonStyles.module.css';
+import './RegisterPage.css';
 
 function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
-  const navigate = useNavigate(); // Initialize useNavigate hook
+  const navigate = useNavigate(); // 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await axios.post(`${process.env.REACT_APP_BACKEND_URL}/register`, { email, password });
       setMessage('Registration successful. Please log in.');
-      // Optionally, navigate to the login page after successful registration
-      setTimeout(() => navigate('/login'), 3000); // Use navigate function to redirect
+
+      setTimeout(() => navigate('/login'), 3000); 
     } catch (error) {
       setMessage('An error occurred. Please try again later.');
     }
   };
 
   return (
-    <div className={styles.container}>
-      <h2>Register</h2>
-      {message && <p>{message}</p>}
-      <form onSubmit={handleSubmit}>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-        <button type="submit">Register</button>
-      </form>
+    <div className="register-container">
+      <div className="register-form">
+        <h2>Register</h2>
+        {message && <p>{message}</p>}
+        <form onSubmit={handleSubmit}>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+                </td>
+              </tr>
+              <tr>
+                <td className="button-container">
+                  <button type="submit">Register</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </form>
+      </div>
+      <div className="register-gif">
+        <img src="https://cdn.dribbble.com/users/988448/screenshots/5240042/icon_cadastro_v5.gif" alt="Registration GIF" />
+      </div>
+      
     </div>
+  
   );
 }
 
